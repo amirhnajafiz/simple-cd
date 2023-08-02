6c64c5816a1ce0230fc08e76d025555ca61b91d9
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2023-08-02 17:53:09 +0330

    Delete note_logs directory

--- note_logs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Notepad, Note
-
-# Register your models here.
-
-admin.site.register(Notepad)
-admin.site.register(Note)
--- note_logs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NoteLogsConfig(AppConfig):
-    name = 'note_logs'
