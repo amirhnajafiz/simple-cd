772ef7051ad1822486515f0e73a58cc153805b7c
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-10 00:32:18 -0400

    update: main logic

--- main.sh
+++ main.sh
@@ -11,29 +11,32 @@ sync="./bin/sync.sh"
 # The main function of simple-cd
 main() {
     # On every main run create a new log entry by printing the date.
-    date >> "$SCD_LOG"
+    date >>"$SCD_LOG"
 
     # First it runs the diff script and export its logs into the log file.
-    $diff >> "$SCD_LOG"
+    $diff >>"$SCD_LOG"
 
     # Capture the exit code of diff script
     status=$?
-    
+
     # Check the exit code.
     if [ $status -eq $EXIT_SUCCESS ]; then
-        # Run the sync script.
-        echo "syncing the application." >> "$SCD_LOG"
-        $sync >> "$SCD_LOG"
+        echo "syncing the application." >>"$SCD_LOG"
+
+        # Run the sync script to update resources.
+        $sync >>"$SCD_LOG"
+
     elif [ $tatus -eq $EXIT_FAILED ]; then
-        echo "failed to run diff script!" >> "$SCD_LOG"
+        echo "failed to run diff script!" >>"$SCD_LOG"
     fi
 }
 
-
 echo "Simple-CD running ..."
+
 # The main loop of simple-cd.
 while true; do
     # Wait for an interval, then run main function.
     sleep $SCD_INTERVAL
+    # Running main function.
     main
 done
