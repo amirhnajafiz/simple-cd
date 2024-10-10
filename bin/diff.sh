771c5b02053251f52b367999c5006582398ce80b
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-09 21:07:49 -0400

    update: diff logic

--- bin/diff.sh
+++ bin/diff.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-# Enter the repo directory
-cd repo/
+# Enter the repo directory, if not exists it will return -1 as an error.
+cd "$SCD_REPO_PATH" || exit -1
 
-# Fetch the latest updates from the remote (without merging or pulling)
-git fetch
+# Fetch the latest updates from the remote (without merging or pulling).
+# If the fetch command failes, it will return an error.
+git fetch || exit -2
 
-# Check if the local branch is behind the remote branch
+# Check if the local branch is behind the remote branch.
 LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse @{u})
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-    echo "New changes detected in the repository!"
+    echo "new changes detected in the repository!"
     
-    # Now pull the changes since we detected updates
-    git pull
+    # Now pull the changes since we detected updates.
+    # If an error happens it will exit with -1.
+    git pull || exit -1
 
     # Exit with status 0 to provide a success message
-    # to the caller (main.sh)
+    # to the caller (main.sh).
     exit 0
 else
-    echo "No new changes."
+    echo "no new changes."
+    # Otherwise, return 1 to caller (main.sh).
+    # It means that no new changes received.
+    exit 1
 fi
-
-# Otherwise, return 1 to caller (main.sh)
-exit 1
