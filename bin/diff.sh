1b0a0c5c81463f25d2194429982b19908da892f1
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-10 00:25:56 -0400

    update: bin systemcalls

--- bin/diff.sh
+++ bin/diff.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Execute sources.
+source ../pkg/status_codes.sh
+
 # Enter the repository directory.
 cd "$SCD_REPO_PATH"
 
 # Fetch the latest updates from the remote (without merging or pulling).
 # If the fetch command failes, it will return an error.
-git fetch || exit 1
+git fetch || exit $EXIT_FAILED
 
 # Check if the local branch is behind the remote branch.
 LOCAL=$(git rev-parse HEAD)
@@ -13,9 +16,9 @@ REMOTE=$(git rev-parse @{u})
 
 if [ "$LOCAL" != "$REMOTE" ]; then   
     # Now pull the changes since we detected updates.
-    # If an error happens it will exit with 1.
-    git pull || exit 0
+    # If succesed, it will exit with success code.
+    git pull && exit $EXIT_SUCCESS
 fi
 
-# When nothing is changed, it returns 2 to the caller (main.sh)
-exit 2
+# When nothing is changed, it returns a none code to the caller (main.sh).
+exit $EXIT_NONE
--- bin/sync.sh
+++ bin/sync.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Build the manifests path
+# Build the manifests path.
 manifests="$SCD_REPO_PATH/$SCD_REPO_MANIFESTS/"
 
-# This will apply kubectl apply -f on all objects
-response=$(kubectl apply -f "$manifests" \
+# This will apply kubectl apply -f on all objects.
+kubectl apply -f "$manifests" \
     --namespace "$SCD_KUBE_NS" \
     --server "$SCD_KUBE_API" \
-    --token "$SCD_KUBE_TOKEN" \
-    2>&1 # capture both stdout and stderr
-)
-
-# Export the response data info log file
-echo "$response" >> "$SCD_LOG"
+    --token "$SCD_KUBE_TOKEN"
