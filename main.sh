8866670a0d00dcb0f680f63f74609b855391a714
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-09 21:15:18 -0400

    add: executeable mode in dockerfile

--- main.sh
+++ main.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Source the `.env` file as environment variables
+source config/.env
+
+# Script variables
+diff="./bin/diff.sh"
+sync="./bin/sync.sh"
+
+# The main function of simple-cd
+main() {
+    # First it runs the diff script.
+    $diff
+}
